4836972caab7f95637be51f1f3f81d6632e53e69
ujjwalmishraazm <ujjwalmishra9598@gmail.com>
2025-09-07 22:37:13 +0530

    motion

--- src/Motion/Content.jsx
+++ src/Motion/Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'motion/react';
+import {motion} from 'motion/react'
 
 const Content = () => {
   return (
@@ -10,11 +10,11 @@ const Content = () => {
         style={{translateZ:100}}
         transition={{ease:"easeInOut",duration:2}}
          className="w-40 h-20 bg-gray-900 text-gray-300 relative group">
-          <p className="group-hover:text-cyan-500 p-3 text-3xl mx-auto">Subscribe</p>
+          <p className="group-hover:text-cyan-500 p-8 text-3xl mx-auto">Subscribe</p>
           {/* Gradient underline */}
           <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-3/4 h-px bg-gradient-to-r from-transparent via-cyan-700 to-transparent"></span>
                <span className="absolute opacity-0 group-hover:opacity-100 bottom-[10px] left-1/2 -translate-x-1/2 w-full h-px bg-gradient-to-r from-transparent via-cyan-700 to-transparent mx-auto blur-sm"></span>
-        </motion.button>
+        </.button>
       </div>
     </div>
   );
