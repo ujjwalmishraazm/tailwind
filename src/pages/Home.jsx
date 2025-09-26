c89b3b058922819d68083f92e70fc1ff4bc526c7
ujjwalmishraazm <ujjwalmishra9598@gmail.com>
2025-09-26 12:46:59 +0530

    fix: correct typo in button label and improve heading formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
                 <div className=" bg-gray-300 border-gray-600 rounded-xl text-gray-600 px-3 py-1 hover:bg-gray-400 transition-colors duration-2000 ease-in-out">
                     <span>Incorporation common mistake to aviod</span>
                 </div>
-                <div> <h1 className="text-5xl font-medium mt-8 text-center tracking-tight text-gray-800   ">Magically Simplify <br /> Accounting And Taxes</h1>
+                <div> <h1 className="text-5xl font-medium mt-8 text-center tracking-tight text-gray-800   ">Magically Simplify <br /> Accounting And Taxes </h1>
+                <h2>this is <menu>buttond  </menu></h2>
                 <p className="text-center max-w-[500px] mt-6 text-gray-500 font-light">  Fintech revolutionizes finance by leveraging technology to make banking, investing, and payments faster, safer, and more accessible. 8<span>:</span>30 </p>
                 <div className="mt-8">
                     <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-2000 ease-in-out">Get Started</button>
